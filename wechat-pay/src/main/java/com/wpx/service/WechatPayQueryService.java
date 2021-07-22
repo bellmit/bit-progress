5cff2d6bc980a1f746e969affd71288916bc6b67
wpx <896590620@qq.com>
2021-07-23 00:07:22 +0800

    fix wechat-pay : 完善订单查询方法

--- wechat-pay/src/main/java/com/wpx/service/WechatPayQueryService.java
+++ wechat-pay/src/main/java/com/wpx/service/WechatPayQueryService.java
@@ -23,14 +23,27 @@ public class WechatPayQueryService {
      * @param mchid  直连商户的商户号，由微信支付生成并下发
      */
     public TransactionQueryResult queryTransactionByTransactionId(String transactionId, String mchid) {
-        StringBuilder builder = new StringBuilder(WechatUrl.TRANSACTION_ID_QUERY_URL);
-        String url = builder.append(transactionId)
-                .append(StringConstants.QUESTION_MARK)
-                .append("mchid=")
-                .append(mchid)
-                .toString();
+        return queryTransaction(WechatUrl.TRANSACTION_ID_QUERY_URL, transactionId, mchid);
+    }
+
+    /**
+     * 根据商家订单号查询订单信息
+     *
+     * @param outTradeNo  商户系统内部订单号，只能是数字、大小写字母_-*且在同一个商户号下唯一
+     * @param mchid  直连商户的商户号，由微信支付生成并下发
+     */
+    public TransactionQueryResult queryTransactionByOutTradeNo(String outTradeNo, String mchid) {
+        return queryTransaction(WechatUrl.OUT_TRADE_NO_QUERY_URL, outTradeNo, mchid);
+    }
+
+    /**
+     * 查询订单信息
+     *
+     * @param url  请求url
+     */
+    public TransactionQueryResult queryTransaction(String url, String pathParam, String mchid) {
         try {
-            String result = HttpClientUtils.doGet(url);
+            String result = HttpClientUtils.doGet(url + pathParam + StringConstants.QUESTION_MARK + "mchid=" + mchid);
             return JSON.parseObject(result, TransactionQueryResult.class);
         } catch (IOException e) {
             e.printStackTrace();
